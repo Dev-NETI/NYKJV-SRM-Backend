01a96c06d900b14cbfbd03054096b5d6bcc63a58
OLIVERALVA <OLIVERALVA@LAPTOP-BDIROJ49.ntma.local>
2024-10-07 15:23:24 +0800

    adding table migration, seeder and button for product

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,16 +24,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'f_name',
-        'm_name',
-        'l_name',
+        'name',
         'email',
         'password',
-        'email_verified',
-        'password',
-        'picture',
-        'provider_id',
-        'provider_token',
     ];
 
     /**
@@ -38,6 +37,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,17 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('f_name')->nullable();
-            $table->string('m_name')->nullable();
-            $table->string('l_name')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
-            $table->boolean('email_verified')->nullable();
-            $table->string('password')->nullable();
-            $table->string('picture')->nullable();
-            $table->string('provider_id')->nullable();
-            $table->string('provider_token')->nullable();
-
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
 
