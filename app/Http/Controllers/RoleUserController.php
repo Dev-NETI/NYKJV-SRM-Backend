c640e08dbe7a77295d3970ff6253f0c101b59d39
NOCDEPT <angeloperia3@gmail.com>
2024-12-06 14:53:39 +0800

    modified role user

--- app/Http/Controllers/RoleUserController.php
+++ app/Http/Controllers/RoleUserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\RoleUser as ModelsRoleUser;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Request as FacadesRequest;
 use Illuminate\Support\Facades\Validator;
 
 class RoleUserController extends Controller
