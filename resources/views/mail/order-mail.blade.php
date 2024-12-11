c3a1c85781e1ac45925dd80502db07bb5a5491e5
Sherwin Christopher Roxas <sherwin.roxas@neti.local>
2024-12-11 10:58:45 +0800

    added email body to upload quotation API

--- resources/views/mail/order-mail.blade.php
+++ resources/views/mail/order-mail.blade.php
@@ -1,8 +1,5 @@
 @include('mail.EmailHeadFormat')
-            <h1>Hi</h1>
-            <p>
-                We are pleased to inform you that a quotation has been received from {{$company}}.
-            </p>
+            {!!$emailBody!!}
             <p>Please log in to the purchasing system to review the quotation at your earliest convenience, or click the link below to view it:</p>
             <p>
                 <a href="{{$quotationUrl}}" target="_blank">View Quotation</a>
